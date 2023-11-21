715cf1234e6024e8ecae20fbe98db6291bf24424
Cokoloof <tagick@gmail.com>
2023-11-21 11:13:51 +0300

    Add filter menu by category

--- xmlconverter/traits/menutrait.php
+++ xmlconverter/traits/menutrait.php
@@ -21,13 +21,26 @@ trait MenuTrait {
         return $menuArray;
     }
 
-    public function getMenuXmlRequest(): string
+    public function getMenuXmlRequest($categoryName = ""): string
     {
         $command = $this->generateDomObjectWithHeader();
         $command->setAttribute("CMD","GetRefData");
         $command->setAttribute("RefName","MenuItems");
         $command->setAttribute("OnlyActive","1");
-
+        //Если задано имя категории блюд, блюда будут фильтроваться по нему
+        if(!empty($categoryName))
+        {
+            $propFilters = $command->createElement("PROPFILTERS");
+            $propFilter = $command->createElement("PROPFILTER");
+            $propFilterInner = $command->createElement("PROPFILTER");
+            $propFilterInner->setAttribute("Name", "CategPath");
+            $propFilterInner->setAttribute("Value", $categoryName);
+            //Разрешение использования регулярных выражений в Value
+            $propFilterInner->setAttribute("Masked", "1");
+            $propFilterInner = $propFilter->appendChild($propFilterInner);
+            $propFilter = $propFilters->appendChild($propFilter);
+            $propFilters = $command->appendChild($propFilters);
+        }
         return $this->saveXml($command);
     }
 }
